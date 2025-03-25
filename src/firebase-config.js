1e1c814ae5cb10e6d55bf68534abfb76ff946536
BNj10 <saquinbenjie7@gmail.com>
2025-03-26 01:19:07 +0800

    Add .env for keys

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkRmu70nvr-HArUXsSlLDJcGB2I9C07zo",
-  authDomain: "rusetada.firebaseapp.com",
-  databaseURL: "https://rusetada-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "rusetada",
-  storageBucket: "rusetada.firebasestorage.app",
-  messagingSenderId: "997724032072",
-  appId: "1:997724032072:web:4693ac6aff1b6d63291c7d"
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
